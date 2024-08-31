d5b21045893fa4bc1415a5e576c1e98267828b54
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-08-31 13:25:36 +0300

    02-01-essentials: refactor code into separate handlers, add note page and addNoteHandler

--- 02-01-essentials/app.js
+++ 02-01-essentials/app.js
@@ -1,46 +1,20 @@
 import express from 'express';
 
-import { HTMX_KNOWLEDGE } from './data/htmx-info.js';
+import {addNoteHandler, newNoteHandler} from "./routes/notes.js";
+import {fetchInfoHandler, mainPageHandler} from "./routes/info.js";
 
 const app = express();
 
+app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>HTMX Essentials</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" href="/icon.png" />
-        <link rel="stylesheet" href="/main.css" />
-        <script type="text/javascript" src="/htmx.min.js" defer></script>
-      </head>
-      <body>
-        <header id="main-header">
-          <img src="/htmx-logo.jpg" alt="HTMX Logo" />
-          <h1>Essentials</h1>
-        </header>
+// Main page
+app.get('/', mainPageHandler);
+app.get('/info', fetchInfoHandler);
 
-        <main>
-          <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
-          <button hx-trigger="click once" hx-get="/info" hx-target="main" hx-swap="beforeend">Learn More</button>
-        </main>
-      </body>
-    </html>
-  `);
-});
+// Notes
+app.get('/new-note', newNoteHandler);
+app.post('/note', addNoteHandler)
 
-app.get('/info', (req, res) => {
-  res.send(`
-  <ul>
-    ${HTMX_KNOWLEDGE.map(info => `<li>${info}</li>`).join('')}
-  </ul>
-  `)
-})
 
 app.listen(3000);
